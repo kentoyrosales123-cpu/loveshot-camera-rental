9bbb18d0e675e70bb11a67fecaaebbe1d78cb537
engistudy <geraldgonzaless123@gmail.com>
2026-05-08 17:40:58 +0800

    Fixed Render deployment

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname)));
 
 mongoose
   .connect(process.env.MONGODB_URI)
