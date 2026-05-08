5f6520982c12eaab1fc23ecd7dd0e053eb73f008
engistudy <geraldgonzaless123@gmail.com>
2026-05-09 06:29:09 +0800

    Fix Gmail SMTP connection

--- server.js
+++ server.js
@@ -12,10 +12,15 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const app = express();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
